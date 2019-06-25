b8ec34e20dab98da9dfaae81bafb123024465052
goncharov <andrewnikolaevich.2014@gmail.com>
2019-06-25 10:28:05 +0300

    new file with styles, new style of page, new header and footer

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Экзамен PHP</title>
+    <title>Заполните форму</title>
 
     <link rel="stylesheet" href="style.css">
     <script src="jquery.min.js"></script>
@@ -15,8 +15,8 @@
 
 <div class="page-layout">
     <header>
-        <a href="" class="logo"><img class="logo-image" src="./assets/logo-polytech.png" alt="Московский Политех"></a>
-        <h1>Финальный экзамен</h1>
+        <a href="" class="logo"><img class="logo-image" src="./assets/logo.png" alt="Этафета мира"></a>
+        <h1>Заполните форму</h1>
     </header>
 
     <main>
